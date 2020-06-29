cd8f51b6bc617db3ad6b2036483ff591d8f42507
MonkeyMomma <monkey_momma08@hotmail.com>
2020-06-29 03:46:10 -0700

    initial commit: API Challenge. Functionality uncertain...

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -17,3 +17,26 @@ function handleSubmit(event) {
 }
 
 export { handleSubmit }
+
+function handleSubmit(event) {
+    event.preventDefault()
+
+    // check what text was put into the form field
+    let formText = document.getElementById('zip').value
+
+    // URL For Weather Data
+    const url = "https://api.openweathermap.org/data/2.5/weather?zip=";
+
+    Client.checkForName(formText)
+
+    console.log("::: Form Submitted :::")
+    fetch('url&zip')
+    .then(res => {
+        return res.json()
+    })
+    .then(function(data) {
+        document.getElementById('results').innerHTML = data.message
+    })
+}
+
+export { handleSubmit }
